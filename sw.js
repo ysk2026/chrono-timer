7e69dff81cdd6c3faeb8a15669206782abcd9cf6
yamamoro <yusuke.otaki@gmail.com>
2026-03-28 19:23:22 +0900

    最初のアップロード

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'chrono-timer-v15';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
